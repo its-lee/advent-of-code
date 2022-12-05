cad4cc54dedc74676636f769a8433a6af1ae4ddc
Cygnut <ligand@hotmail.co.uk>
2022-12-05 18:39:01 +0000

    done both parts, cleaned up

--- src/days/day5.js
+++ src/days/day5.js
@@ -513,39 +513,43 @@ move 5 from 7 to 9
 move 27 from 7 to 5
 move 2 from 9 to 6`;
 
-const [initialState, moves] = input.split('\n\n');
+const [stringizedInitialState, moves] = input.split('\n\n');
 
-const stateRows = initialState.split('\n').slice(0, -1).reverse();
-const emptyState = stateRows[0]
-  .split('')
-  .filter(c => c === '[')
-  .map(() => []);
+const computeInitialState = () => {
+  const stateRows = stringizedInitialState.split('\n').slice(0, -1).reverse();
+  const emptyState = stateRows[0]
+    .split('')
+    .filter(c => c === '[')
+    .map(() => []);
 
-const state = stateRows.reduce((acc, line) => {
-  line.split('').forEach((c, i) => {
-    if (i % 4 === 1 && c.trim() !== '') {
-      acc[Math.floor(i / 4)].push(c);
-    }
-  });
+  return stateRows.reduce((acc, line) => {
+    line.split('').forEach((c, i) => {
+      if (i % 4 === 1 && c.trim() !== '') {
+        acc[Math.floor(i / 4)].push(c);
+      }
+    });
 
-  return acc;
-}, emptyState);
+    return acc;
+  }, emptyState);
+};
 
-const r = new RegExp(`^${'\\D+(\\d+)'.repeat(3)}$`);
+const initialState = computeInitialState();
+const moveRegex = new RegExp(`^${'\\D+(\\d+)'.repeat(3)}$`);
 
-export default [
-  moves,
-  moves
+const computeMutatedTop = reverse => {
+  return moves
     .split('\n')
     .reduce((state, move) => {
-      const [, amount, from, to] = r.exec(move).map(v => parseInt(v));
+      const [, amount, from, to] = moveRegex.exec(move).map(v => parseInt(v));
       const moving = state[from - 1].splice(-amount, amount);
-      state[to - 1].push(...moving.reverse());
+      state[to - 1].push(...(reverse ? moving.reverse() : moving));
       return state;
-    }, state)
+    }, initialState)
     .reduce((acc, column) => {
       acc.push(column[column.length - 1]);
       return acc;
     }, [])
-    .reduce((acc, v) => acc + v)
-];
+    .reduce((acc, v) => acc + v);
+};
+
+export default [computeMutatedTop(true), computeMutatedTop(false)];
