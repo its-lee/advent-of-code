57d6caee1e2e7ded37dacc173ed5220464ff8fa9
Cygnut <ligand@hotmail.co.uk>
2022-12-08 22:11:04 +0000

    do it the dumb way and you get an answer

--- src/days/day2/index.js
+++ src/days/day2/index.js
@@ -4,6 +4,12 @@ import { charCode } from '../../helpers/utility.js';
 import { sumReducer } from '../../helpers/reducers.js';
 
 export default day(({ answer, source }) => {
+  const parsedSource = source.split('\n').map(l => {
+    let [a, b] = l.toLowerCase().split(' ').map(charCode);
+    b -= 23;
+    return [a, b];
+  });
+
   const gameScore = (them, us) => {
     // winDrawLose = -1 = lose, 0 = draw, 1 = win
     let winDrawLose = (3 + us - them) % 3;
@@ -11,15 +17,18 @@ export default day(({ answer, source }) => {
     return (winDrawLose + 1) * 3;
   };
 
+  const potentialMoves = [1, 2, 3];
+
+  answer(parsedSource.map(([them, us]) => us + gameScore(them, us)).reduce(...sumReducer()), 15523);
+
   answer(
-    source
-      .split('\n')
-      .map(l => {
-        let [them, us] = l.toLowerCase().split(' ').map(charCode);
-        us -= 23;
-        return us + gameScore(them, us);
+    parsedSource
+      .map(([them, scoreHint]) => {
+        const requiredGameScore = (scoreHint - 1) * 3;
+        const moveScore = potentialMoves.find(us => gameScore(them, us) === requiredGameScore);
+        return requiredGameScore + moveScore;
       })
       .reduce(...sumReducer()),
-    15523
+    15702
   );
 });
