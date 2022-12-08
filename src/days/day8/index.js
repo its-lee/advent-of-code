ff66da9336562464bdcbec5f9b2a306993accac8
Cygnut <ligand@hotmail.co.uk>
2022-12-08 22:54:29 +0000

    part 1

--- src/days/day8/index.js
+++ src/days/day8/index.js
@@ -2,10 +2,20 @@ import day from '../../helpers/day.js';
 
 export default day(({ answer, source }) => {
   const grid = source.split('\n').map(row => row.split('').map(v => parseInt(v)));
+  const cells = grid.flatMap((row, y) => row.map((value, x) => ({ value, x, y })));
 
-  const cells = grid.map((row, rowIndex) => {
-    return row.flatMap((value, columnIndex) => ({ value, row: rowIndex, column: columnIndex }));
+  const visible = cells.filter(({ value, x, y }) => {
+    const tallerOrEqualCells = cells.filter(c => c.value >= value);
+
+    const directions = [
+      tallerOrEqualCells.filter(c => c.y === y && c.x < x),
+      tallerOrEqualCells.filter(c => c.y === y && c.x > x),
+      tallerOrEqualCells.filter(c => c.x === x && c.y > y),
+      tallerOrEqualCells.filter(c => c.x === x && c.y < y)
+    ];
+
+    return directions.some(d => d.length === 0);
   });
 
-  console.log(cells);
+  answer(visible.length);
 });
