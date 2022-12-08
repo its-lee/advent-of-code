8fe4fad9166c394d6c7315bb89788dfe25bb656d
Cygnut <ligand@hotmail.co.uk>
2022-12-08 19:40:15 +0000

    partial new command

---
+++ src/commands.js
@@ -0,0 +1,66 @@
+import { range } from './helpers.js';
+
+const getDaySubmission = async day => (await import(`./days/day${day}/index.js`)).default;
+
+const getAllDaySubmissions = async () => {
+  const loaded = {};
+  for (const day of range(1, 25)) {
+    loaded[day] = {};
+    try {
+      loaded[day].submission = await getDaySubmission(day);
+    } catch (e) {
+      // ignore instances where we haven't finished that test yet..
+      if (!(e instanceof Error && e.code === 'ERR_MODULE_NOT_FOUND')) {
+        loaded[day].error = `An error occurred while importing day #${day} - ${e}`;
+      }
+    }
+  }
+
+  return loaded;
+};
+
+const requireDayParameter = day => {
+  if (!day || isNaN(parseInt(day))) {
+    throw new Error('A numeric day parameter is required');
+  }
+};
+
+export const handleDayCommand = async ([day]) => {
+  requireDayParameter(day);
+
+  const submission = await getDaySubmission(day);
+  console.log(submission.items);
+};
+
+export const handleTestCommand = async () => {
+  const fails = [];
+  const addFail = (day, message) => fails.push(`Day #${day}: ${message}`);
+
+  const loaded = await getAllDaySubmissions();
+  Object.entries(loaded).forEach(([day, { submission, error }]) => {
+    if (error) {
+      addFail(day, error);
+    }
+
+    if (!submission) {
+      return;
+    }
+
+    submission.items.forEach(({ actual, expected }) => {
+      if (expected !== undefined && expected !== actual) {
+        addFail(day, `Returns ${actual} != ${expected}`);
+      }
+    });
+  });
+
+  fails.forEach(f => console.error(f));
+  if (fails.length) {
+    throw new Error('There are failing tests');
+  }
+};
+
+export const handleNewCommand = ([day]) => {
+  requireDayParameter(day);
+
+  console.log(day);
+};
--- src/index.js
+++ src/index.js
@@ -1,65 +1,24 @@
-import { range } from './helpers.js';
+import { handleDayCommand, handleTestCommand, handleNewCommand } from './commands.js';
 
-const getDaySubmission = async day => (await import(`./days/day${day}/index.js`)).default;
-
-const getAllDaySubmissions = async () => {
-  const loaded = {};
-  for (const day of range(1, 25)) {
-    loaded[day] = {};
-    try {
-      loaded[day].submission = await getDaySubmission(day);
-    } catch (e) {
-      // ignore instances where we haven't finished that test yet..
-      if (!(e instanceof Error && e.code === 'ERR_MODULE_NOT_FOUND')) {
-        loaded[day].error = `An error occurred while importing day #${day} - ${e}`;
-      }
-    }
-  }
-
-  return loaded;
-};
-
-const handleDayCommand = async args => {
-  const [day] = args;
-  const submission = await getDaySubmission(day);
-  console.log(submission.items);
-};
-
-const handleTestCommand = async () => {
-  const fails = [];
-  const addFail = (day, message) => fails.push(`Day #${day}: ${message}`);
-
-  const loaded = await getAllDaySubmissions();
-  Object.entries(loaded).forEach(([day, { submission, error }]) => {
-    if (error) {
-      addFail(day, error);
-    }
-
-    if (!submission) {
-      return;
-    }
-
-    submission.items.forEach(({ actual, expected }) => {
-      if (expected !== undefined && expected !== actual) {
-        addFail(day, `Returns ${actual} != ${expected}`);
-      }
-    });
-  });
-
-  fails.forEach(f => console.error(f));
-  process.exit(fails.length ? 1 : 0);
+const commands = {
+  day: handleDayCommand,
+  test: handleTestCommand,
+  new: handleNewCommand
 };
 
 const [command, ...args] = process.argv.slice(2);
-
-const requestedCommand = {
-  day: handleDayCommand,
-  test: handleTestCommand
-}[command];
+const requestedCommand = commands[command];
 
 if (!requestedCommand) {
   console.error(`Unsupported command '${command}'`);
   process.exit(1);
 }
 
-requestedCommand(args);
+(async () => {
+  try {
+    await requestedCommand(args);
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+})();
