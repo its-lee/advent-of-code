19d7e7e1c5489b2f02531a858a2d393e493ead8b
Cygnut <ligand@hotmail.co.uk>
2022-12-08 23:24:25 +0000

    yay part 2

--- src/days/day8/index.js
+++ src/days/day8/index.js
@@ -4,39 +4,34 @@ export default day(({ answer, source }) => {
   const grid = source.split('\n').map(row => row.split('').map(v => parseInt(v)));
   const cells = grid.flatMap((row, y) => row.map((value, x) => ({ value, x, y })));
 
-  const countVisibleCells = () => {
-    return cells.filter(({ value, x, y }) => {
-      const tallerOrEqualCells = cells.filter(c => c.value >= value);
-
-      const directions = [
-        tallerOrEqualCells.filter(c => c.y === y && c.x < x),
-        tallerOrEqualCells.filter(c => c.y === y && c.x > x),
-        tallerOrEqualCells.filter(c => c.x === x && c.y > y),
-        tallerOrEqualCells.filter(c => c.x === x && c.y < y)
-      ];
-
-      return directions.some(d => d.length === 0);
-    }).length;
-  };
-
-  answer(countVisibleCells(), 1796);
-
   const by =
     (getter, reverse = false) =>
     (a, b) =>
       (reverse ? -1 : 1) * (getter(a) - getter(b));
 
-  const scenicScoreCells = () => {
-    // Order is important here (it wasn't before!)
-    return cells.map(({ value, x, y }) => {
-      const directions = [
-        cells.filter(c => c.y === y && c.x < x).sort(by(v => v.x, true)),
-        cells.filter(c => c.y === y && c.x > x).sort(by(v => v.x, false)),
-        cells.filter(c => c.x === x && c.y > y).sort(by(v => v.y, false)),
-        cells.filter(c => c.x === x && c.y < y).sort(by(v => v.y, true))
-      ];
+  const computeDirections = (cells, x, y) => {
+    // Order is important for part 2, we order so the arrays start from (x, y) and
+    // spread radially outwards.
+    return [
+      cells.filter(c => c.y === y && c.x < x).sort(by(v => v.x, true)),
+      cells.filter(c => c.y === y && c.x > x).sort(by(v => v.x, false)),
+      cells.filter(c => c.x === x && c.y > y).sort(by(v => v.y, false)),
+      cells.filter(c => c.x === x && c.y < y).sort(by(v => v.y, true))
+    ];
+  };
 
-      const score = directions
+  const countVisibleCells = () => {
+    return cells.filter(({ value, x, y }) => {
+      const tallerOrEqualCells = cells.filter(c => c.value >= value);
+      return computeDirections(tallerOrEqualCells, x, y).some(d => d.length === 0);
+    }).length;
+  };
+
+  answer(countVisibleCells(), 1796);
+
+  const scoreCells = () => {
+    return cells.map(({ value, x, y }) => {
+      const score = computeDirections(cells, x, y)
         .map(d => {
           const index = d.findIndex(c => value <= c.value);
           return index < 0 ? d.length : index + 1;
@@ -47,5 +42,5 @@ export default day(({ answer, source }) => {
     });
   };
 
-  answer(Math.max(...scenicScoreCells()), 288120);
+  answer(Math.max(...scoreCells()), 288120);
 });
