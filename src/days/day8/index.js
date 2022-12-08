82df7be8d670b839e0c9790f345cb33c05b9b49d
Cygnut <ligand@hotmail.co.uk>
2022-12-08 22:56:12 +0000

    cleanup

--- src/days/day8/index.js
+++ src/days/day8/index.js
@@ -4,18 +4,20 @@ export default day(({ answer, source }) => {
   const grid = source.split('\n').map(row => row.split('').map(v => parseInt(v)));
   const cells = grid.flatMap((row, y) => row.map((value, x) => ({ value, x, y })));
 
-  const visible = cells.filter(({ value, x, y }) => {
-    const tallerOrEqualCells = cells.filter(c => c.value >= value);
+  const countVisibleCells = () => {
+    return cells.filter(({ value, x, y }) => {
+      const tallerOrEqualCells = cells.filter(c => c.value >= value);
 
-    const directions = [
-      tallerOrEqualCells.filter(c => c.y === y && c.x < x),
-      tallerOrEqualCells.filter(c => c.y === y && c.x > x),
-      tallerOrEqualCells.filter(c => c.x === x && c.y > y),
-      tallerOrEqualCells.filter(c => c.x === x && c.y < y)
-    ];
+      const directions = [
+        tallerOrEqualCells.filter(c => c.y === y && c.x < x),
+        tallerOrEqualCells.filter(c => c.y === y && c.x > x),
+        tallerOrEqualCells.filter(c => c.x === x && c.y > y),
+        tallerOrEqualCells.filter(c => c.x === x && c.y < y)
+      ];
 
-    return directions.some(d => d.length === 0);
-  });
+      return directions.some(d => d.length === 0);
+    }).length;
+  };
 
-  answer(visible.length);
+  answer(countVisibleCells(), 1796);
 });
