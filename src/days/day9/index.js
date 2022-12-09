320fd5e478209f6f27467e4216ed2ed52d0b07a1
Cygnut <ligand@hotmail.co.uk>
2022-12-09 18:00:02 +0000

    more cleanup

--- src/days/day9/index.js
+++ src/days/day9/index.js
@@ -29,40 +29,50 @@ export default day(({ answer, source }) => {
 
   const infinityNorm = p => Math.max(...p.map(v => Math.abs(v))); // ayyyy it's our good friend the L-infinity norm!
   const relative = (a, b) => b.map((v, i) => v - a[i]);
+
+  const moveTail = (head, tail) => {
+    const tailRelativeToHead = relative(tail, head);
+
+    if (infinityNorm(tailRelativeToHead) <= 1) {
+      return tail;
+    }
+
+    // The head will only be in positions indicated by 'h' below (as we will always be catching up
+    // so we're either diagonally or cardinally adjacent - and the head has only moved once).
+    //       hhh
+    //       ..h
+    //       T.h
+    //   Soooo.. we can work out how the tail should move by looking at the relative vector - just
+    // find any instances of where it'd move by > 1 in a direction and snub that down to 1.
+    const tailMove = tailRelativeToHead.map(v =>
+      Math.abs(v) > 1 ? Math.sign(v) * (Math.abs(v) - 1) : v
+    );
+
+    return move(tail, tailMove);
+  };
+
   const dedupePositions = array => dedupe(array, (a, b) => a.every((v, i) => v === b[i]));
 
-  const computeTailPositions = () => {
+  const computeTailVisits = () => {
     let tail = [0, 0];
     const tailPositions = [];
 
     headPositions.forEach(head => {
-      const tailRelativeToHead = relative(tail, head);
-
-      if (infinityNorm(tailRelativeToHead) > 1) {
-        // The head will only be in positions indicated by 'h' below (as we will always be catching up
-        // so we're either diagonally or cardinally adjacent - and the head has only moved once).
-        //       hhh
-        //       ..h
-        //       T.h
-        //   Soooo.. we can work out how the tail should move by looking at the relative vector - just
-        // find any instances of where it'd move by > 1 in a direction and snub that down to 1.
-        const tailMove = tailRelativeToHead.map(v =>
-          Math.abs(v) > 1 ? Math.sign(v) * (Math.abs(v) - 1) : v
-        );
-        tail = move(tail, tailMove);
-      }
-
+      tail = moveTail(head, tail);
       tailPositions.push(tail);
     });
 
-    return dedupePositions(tailPositions);
+    return dedupePositions(tailPositions).length;
   };
 
-  answer(computeTailPositions().length, 5960);
+  answer(computeTailVisits(), 5960);
 
-  const computeLongerTailPositions = () => {
-    return [];
+  const computeLongerTailVisits = () => {
+    let tail = range(1, 9).map(() => [0, 0]);
+    return [].length;
   };
 
-  answer(computeLongerTailPositions().length);
+  // todo: pass length to the above & use it in both answers!
+
+  answer(computeLongerTailVisits());
 });
