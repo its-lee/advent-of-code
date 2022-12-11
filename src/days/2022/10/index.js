432681d01a2fa628b7826ce12188a1e5afacd6d5
Cygnut <ligand@hotmail.co.uk>
2022-12-11 22:24:49 +0000

    cleanup

--- src/days/2022/10/index.js
+++ src/days/2022/10/index.js
@@ -4,30 +4,30 @@ import day from '../../../runner/day.js';
 export default day(({ answer, source }) => {
   const commands = source.split('\n');
 
-  let register = 1;
-  const registerHistory = commands.flatMap(c => {
-    const resolved = c === 'noop' ? ['noop'] : [c, 'noop'];
-    let change = 0;
-    const match = /addx (.*)/.exec(c);
-    if (match) {
-      [, change] = match.map(v => parseInt(v));
-    }
+  const computeRegisterValues = () => {
+    let register = 1;
+    return commands.flatMap(c => {
+      const resolved = c === 'noop' ? ['noop'] : [c, 'noop'];
+      let change = 0;
+      const match = /addx (.*)/.exec(c);
+      if (match) {
+        [, change] = match.map(v => parseInt(v));
+      }
 
-    const history = resolved.map(() => register);
-    register += change;
+      const history = resolved.map(() => register);
+      register += change;
 
-    return history;
-  });
+      return history;
+    });
+  };
 
-  const periodicMeasurements = registerHistory.reduce((acc, value, index) => {
-    const cycle = index + 1;
-    const isntInteresting = (cycle - 20) % 40;
-    return acc + (isntInteresting ? 0 : cycle * value);
-  }, 0);
+  const computeInterestingScore = () => {
+    return computeRegisterValues().reduce((acc, value, index) => {
+      const cycle = index + 1;
+      const isntInteresting = (cycle - 20) % 40;
+      return acc + (isntInteresting ? 0 : cycle * value);
+    }, 0);
+  };
 
-  console.log(registerHistory.length);
-  console.log(registerHistory);
-  console.log(periodicMeasurements);
-
-  answer(periodicMeasurements);
+  answer(computeInterestingScore());
 });
