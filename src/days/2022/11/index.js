543b715b46532b5fcd9e50f403c315230247180c
Cygnut <ligand@hotmail.co.uk>
2022-12-11 23:04:30 +0000

    wip part 1

---
+++ src/days/2022/11/index.js
@@ -0,0 +1,34 @@
+import day from '../../../runner/day.js';
+
+export default day(({ answer, source }) => {
+  const readLastInt = str =>
+    str
+      .split(' ')
+      .slice(-1)
+      .map(v => parseInt(v))[0];
+
+  const computeOperand = (old, str) => (str === 'old' ? old : parseInt(str));
+
+  const monkeys = source.split('\n\n').map(m => {
+    const [, itemsStr, operationStr, divisorStr, trueStr, falseStr] = m.split('\n');
+
+    const [left, operator, right] = operationStr.split(' = ').slice(-1).split(' ');
+
+    return {
+      items: itemsStr
+        .split(': ')
+        .slice(-1)
+        .split(', ')
+        .map(v => parseInt(v)),
+      operation: old => {
+        const l = computeOperand(old, left);
+        const r = computeOperand(old, right);
+        return operator === '*' ? l * r : l + r;
+      },
+      divisor: readLastInt(divisorStr),
+      dispatch: [readLastInt(falseStr), readLastInt(trueStr)]
+    };
+  });
+
+  console.log(monkeys);
+});
--- src/days/index.js
+++ src/days/index.js
@@ -9,6 +9,7 @@ import { default as index20227 } from './2022/7/index.js';
 import { default as index20228 } from './2022/8/index.js';
 import { default as index20229 } from './2022/9/index.js';
 import { default as index202210 } from './2022/10/index.js';
+import { default as index202211 } from './2022/11/index.js';
 
 const days = {
   '2022/1': index20221,
@@ -20,7 +21,8 @@ const days = {
   '2022/7': index20227,
   '2022/8': index20228,
   '2022/9': index20229,
-  '2022/10': index202210
+  '2022/10': index202210,
+  '2022/11': index202211
 };
 
 export default days;
