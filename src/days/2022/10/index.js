eb8851a191bf4402759f7807cbf9040d5c73987f
Cygnut <ligand@hotmail.co.uk>
2022-12-11 22:49:25 +0000

    add answer

--- src/days/2022/10/index.js
+++ src/days/2022/10/index.js
@@ -1,8 +1,9 @@
-import { range } from '../../../helpers/utility.js';
 import day from '../../../runner/day.js';
+import { range } from '../../../helpers/utility.js';
 
 export default day(({ answer, source }) => {
   const commands = source.split('\n');
+  const SCREEN_WIDTH = 40;
 
   const computeRegisterValues = () => {
     let register = 1;
@@ -24,10 +25,30 @@ export default day(({ answer, source }) => {
   const computeInterestingScore = () => {
     return computeRegisterValues().reduce((acc, value, index) => {
       const cycle = index + 1;
-      const isntInteresting = (cycle - 20) % 40;
+      const isntInteresting = (cycle - SCREEN_WIDTH / 2) % SCREEN_WIDTH;
       return acc + (isntInteresting ? 0 : cycle * value);
     }, 0);
   };
 
   answer(computeInterestingScore());
+
+  const computeDisplay = () => {
+    const rows = [];
+    computeRegisterValues().forEach((register, cycle) => {
+      const rowIndex = Math.floor(cycle / SCREEN_WIDTH);
+      const columnIndex = cycle % SCREEN_WIDTH;
+
+      if (rowIndex >= rows.length) {
+        rows.push([]);
+      }
+
+      rows[rowIndex][columnIndex] = range(register - 1, 3).includes(columnIndex) ? '#' : '.';
+    });
+
+    return rows.map(row => row.join('')).join('\n');
+  };
+
+  // console.log(computeDisplay());
+  // The actual answer (which you need to use your human eyes to see) is: BACEKLHF
+  answer(computeDisplay());
 });
