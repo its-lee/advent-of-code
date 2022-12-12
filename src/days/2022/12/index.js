b4eb423305bd3dea8bd9a4f110b6ae2d3e3b5ec0
lee <lee@feeditback.co.uk>
2022-12-12 17:30:49 +0000

    part 2 done - yet more cleaning up

--- src/days/2022/12/index.js
+++ src/days/2022/12/index.js
@@ -43,6 +43,15 @@ export default day(({ answer, source }) => {
       },
       // Note that there may be multiple shortest-paths, this algoritm just returns the first detected.
       // Originally aided by the docs on https://jarednielsen.com/data-structure-graph-shortest-path/
+      //
+      // A breadth-first search (a tree-like algorithm) finds the shortest path by virtue of:
+      //   1. Finding all paths
+      //   2. The order in which we search means we find a path of shortest length first (amongst
+      //      paths that we find to terminate)
+      //
+      // We essentially look at all length 1 paths from the start, then length 2 etc etc and check
+      // each time if they reach the end. Because we're looking in path length order, we'll have the
+      // shortest path if we find one.
       compute(start, end) {
         const queue = [start];
         const visited = { [start]: true };
