c26a9baac544a62d0c1c4c2c40f1a6b4731a954b
Cygnut <ligand@hotmail.co.uk>
2022-12-12 22:44:14 +0000

    cleaning

--- src/days/2022/7/index.js
+++ src/days/2022/7/index.js
@@ -1,51 +1,46 @@
 import day from '../../../runner/day.js';
 
 export default day(({ answer, source }) => {
-  let currentPath = [];
-  const pathSizes = {};
+  const getNonRecursiveDirectorySizes = () => {
+    let currentPath = [];
+    const pathSizes = {};
 
-  source.split('\n').forEach(l => {
-    //console.log(l);
-    if (l.startsWith('$')) {
-      // cd or ls
-      const [, dir] = /^\$ cd (.*)$/.exec(l) || [];
-      if (dir) {
-        if (dir !== '..') {
-          currentPath.push(dir === '/' ? '' : dir);
-        } else {
-          currentPath.pop();
+    source.split('\n').forEach(l => {
+      if (l.startsWith('$')) {
+        const [, dir] = /^\$ cd (.*)$/.exec(l) || [];
+        if (dir) {
+          if (dir !== '..') {
+            currentPath.push(dir === '/' ? '' : dir);
+          } else {
+            currentPath.pop();
+          }
+        }
+      } else {
+        if (!l.startsWith('dir')) {
+          const [, size] = /^(\d+) (.*)$/.exec(l) || [];
+          const pathStr = currentPath.join('/');
+          pathSizes[pathStr] = (pathSizes[pathStr] || 0) + parseInt(size);
         }
       }
-    } else {
-      const [, dir] = /^dir (.*)$/.exec(l) || [];
-      if (dir) {
-        // we don't care about these?
-        // currentDir;
-        // dir[1] is the dir name
-      } else {
-        const [, size] = /^(\d+) (.*)$/.exec(l) || [];
-        const pathStr = currentPath.join('/');
-        pathSizes[pathStr] = (pathSizes[pathStr] || 0) + parseInt(size);
-      }
-    }
-  });
+    });
 
-  const recursiveSizes = Object.keys(pathSizes).reduce((acc, path) => {
-    const recursiveSize = Object.keys(pathSizes).reduce((overall, subPath) => {
-      if (path === '/ftj' && subPath.startsWith(path)) {
-        //console.log('for path', path, 'found subPath', subPath, pathSizes[subPath]);
-      }
+    return pathSizes;
+  };
 
-      return overall + (subPath.startsWith(path) ? pathSizes[subPath] : 0);
-    }, 0);
+  const getRecursivePathSizes = () => {
+    const pathSizes = getNonRecursiveDirectorySizes();
 
-    return { ...acc, [path]: recursiveSize };
-  }, {});
+    return Object.keys(pathSizes).reduce((acc, path) => {
+      const recursiveSize = Object.keys(pathSizes).reduce((overall, subPath) => {
+        return overall + (subPath.startsWith(path) ? pathSizes[subPath] : 0);
+      }, 0);
 
-  const sizeUnder100000 = Object.values(recursiveSizes).filter(size => size <= 100000);
+      return { ...acc, [path]: recursiveSize };
+    }, {});
+  };
+
+  const sizeUnder100000 = Object.values(getRecursivePathSizes()).filter(size => size <= 100000);
   //.reduce((acc, size) => acc + size, 0);
-
-  // path === '/ftj' is too small
 
   answer(sizeUnder100000);
 });
