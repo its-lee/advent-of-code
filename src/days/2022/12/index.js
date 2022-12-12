330db3f570ec6da2344696d17c7a33b71ba1d17b
lee <lee@feeditback.co.uk>
2022-12-12 14:42:00 +0000

    works on the demo

--- src/days/2022/12/index.js
+++ src/days/2022/12/index.js
@@ -8,11 +8,20 @@ export default day(({ answer, source }) => {
   const grid = source
     .split('\n')
     .map(row => row.split(''))
-    .flatMap((row, y) => row.map((s, x) => ({ value: charCode(s), position: [x, y] })));
+    .flatMap((row, y) =>
+      row.map((s, x) => ({
+        mark: s,
+        value: charCode(s === 'E' ? 'z' : s),
+        position: [x, y]
+      }))
+    );
 
-  const initialPath = [grid.find(({ value }) => value === charCode('S'))];
+  const locateByMark = m => grid.find(({ mark }) => mark === m);
 
-  const isDestination = c => c.value === charCode('E');
+  const initialPath = [locateByMark('S')];
+  const end = locateByMark('E');
+
+  const isDestination = c => areEqualVectors(c.position, end.position);
 
   const areAdjacent = (a, b) =>
     relative(a, b)
@@ -23,7 +32,7 @@ export default day(({ answer, source }) => {
     const current = path[path.length - 1];
     return grid
       .filter(c => areAdjacent(current.position, c.position))
-      .filter(c => c.value <= current.value + 1 || isDestination(c)) // ignore cells which are too step
+      .filter(c => c.value <= current.value + 1) // ignore cells which are too step
       .filter(c => !path.some(p => areEqualVectors(p.position, c.position))); // don't go where we've already gone to avoid infinite cycles
   };
 
@@ -49,11 +58,9 @@ export default day(({ answer, source }) => {
 
   const recursePaths = (paths, succeedingPaths = []) => {
     paths.forEach(path => {
-      // console.log(path[path.length - 1].value, 'E=', charCode('E'));
       // check if we're at the end
       if (isDestination(path[path.length - 1])) {
         succeedingPaths.push(path);
-        //console.log('found successful path - length', path.length);
         return;
       }
 
@@ -77,13 +84,13 @@ export default day(({ answer, source }) => {
         range(0, width)
           .map((_, x) => {
             const cell = path.find(p => areEqualVectors(p.position, [x, y]));
-            return cell ? String.fromCharCode(97 + cell.value) : '.';
+            return cell ? String.fromCharCode(96 + cell.value) : '.';
           })
           .join('')
       );
     });
 
-    console.log(path.length);
+    console.log(path.length - 1); // we don't include the start
   };
 
   recursePaths([initialPath])
