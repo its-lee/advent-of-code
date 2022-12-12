54101be8210dc7f5124dd41be97828063e720565
lee <lee@feeditback.co.uk>
2022-12-12 13:19:56 +0000

    linearize

--- src/days/2022/12/index.js
+++ src/days/2022/12/index.js
@@ -1,11 +1,20 @@
 import day from '../../../runner/day.js';
 
-import { transposeMatrix } from '../../../helpers/utility.js';
+import { charCode } from '../../../helpers/utility.js';
 
 export default day(({ answer, source }) => {
-  const grid = transposeMatrix(source.split('\n').map(l => l.split('')));
+  // TODO: dedupe from #8, move value map to end
+  const grid = source
+    .split('\n')
+    .map(row => row.split('').map(v => charCode(v)))
+    .flatMap((row, y) => row.map((value, x) => ({ value, x, y })));
+
+  //const grid = transposeMatrix(source.split('\n').map(l => l.split('')));
 
   // grid[x][y]
 
+  const start = grid.find(({ value }) => value === charCode('S'));
+
   console.log(grid);
+  console.log(start);
 });
