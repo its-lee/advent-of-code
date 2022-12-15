5e88044f68d7e9c0f0239abb01bb1034537cd83a
Cygnut <ligand@hotmail.co.uk>
2022-12-15 20:05:12 +0000

    cleanup

--- src/days/2022/14/index.js
+++ src/days/2022/14/index.js
@@ -1,5 +1,6 @@
 import day from '../../../runner/day.js';
 import { range } from '../../../helpers/utility.js';
+import { relative, addVectors } from '../../../helpers/vector.js';
 
 export default day(source => {
   const CONTENT = {
@@ -8,27 +9,29 @@ export default day(source => {
     SAND: 'o'
   };
 
-  const rockVectors = source
+  const rocks = source
     .split('\n')
     .flatMap(line => {
       const parts = line.split(' -> ');
       return parts.slice(0, -1).map((p, i) => [p, parts[i + 1]]);
     })
-    .map(pair => pair.map(coord => coord.split(',').map(v => parseInt(v))));
+    .map(pair => pair.map(coord => coord.split(',').map(v => parseInt(v))))
+    .flatMap(([a, b]) => {
+      const r = relative(a, b);
+      const changingIndex = r.findIndex(Boolean);
+      const changedValue = r[changingIndex];
 
-  const maxOrdinate = (vectors, index) => Math.max(...vectors.flatMap(v => v).map(v => v[index]));
-  const width = maxOrdinate(rockVectors, 0);
-  const height = maxOrdinate(rockVectors, 1);
+      return range(0, Math.abs(changedValue) + 1).map(v => {
+        const p = [0, 0];
+        p[changingIndex] = Math.sign(changedValue) * v;
+        return addVectors(a, p);
+      });
+    });
+
+  const maxOrdinates = vectors => [0, 1].map(index => Math.max(...vectors.map(v => v[index])));
+  const [width, height] = maxOrdinates(rocks);
 
   const grid = range(0, width).map(() => range(0, height).map(() => CONTENT.AIR));
 
-  rockVectors.forEach(([a, b]) => {
-    const diff = [a[0] !== b[0], a[1] !== b[1]];
-
-    if (diff.filter(Boolean).length === 2) {
-      console.log('hmmm', a, b);
-    }
-  });
-
-  //console.log(grid);
+  console.log(rocks, width, height);
 });
--- src/helpers/vector.js
+++ src/helpers/vector.js
@@ -3,3 +3,5 @@ export const areEqualVectors = (a, b) => a.every((v, i) => v === b[i]);
 export const infinityNorm = p => Math.max(...p.map(v => Math.abs(v))); // Ayyyy it's our good friend the L-infinity norm!
 
 export const relative = (a, b) => b.map((v, i) => v - a[i]);
+
+export const addVectors = (a, b) => b.map((v, i) => v + a[i]);
