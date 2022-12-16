c7c0521b8633fd27909ac42217cd9347c2e2f878
Cygnut <ligand@hotmail.co.uk>
2022-12-16 00:55:49 +0000

    part 1 done

--- src/days/2022/15/index.js
+++ src/days/2022/15/index.js
@@ -1,48 +1,70 @@
 import day from '../../../runner/day.js';
-import {
-  subtractVectors,
-  manhattanNorm,
-  addVectors,
-  computeDimensions
-} from '../../../helpers/vector.js';
+import { subtractVectors, manhattanNorm } from '../../../helpers/vector.js';
+import { range } from '../../../helpers/utility.js';
 
 export default day(({ source }) => {
-  const buildCircle = (centre, radius) => {
-    let circle = [];
-    for (let x = -radius; x <= radius; ++x) {
-      const height = radius - Math.abs(x);
-      for (let y = -height; y <= height; ++y) {
-        circle.push(addVectors(centre, [x, y]));
-      }
+  const readings = source.split('\n').map(l => {
+    const [, sx, sy, bx, by] =
+      /^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/
+        .exec(l)
+        .map(v => parseInt(v));
+
+    return {
+      sensor: [sx, sy],
+      beacon: [bx, by]
+    };
+  });
+
+  const intersectCircleWithLine = (centre, radius, lineY) => {
+    const yDistance = Math.abs(lineY - centre[1]);
+    if (yDistance > radius) {
+      return [];
     }
 
-    return circle;
+    const xRadius = 2 * radius + 1 - 2 * yDistance;
+    return range(centre[0] - (radius - yDistance), xRadius);
+
+    /*
+      radius 2
+      yDistance  
+        0 - fine (5 wide),              c - 2
+        1 (below) - fine (3 wide),      c - 1
+        2 - fine (1 wide),              c - 0
+        >= 3 bad
+
+            *   
+      -----+++------
+          *****
+           ***
+            *
+    */
   };
 
-  const sensorCircles = source
-    .split('\n')
-    .map(l => {
-      const [, sx, sy, bx, by] =
-        /^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/
-          .exec(l)
-          .map(v => parseInt(v));
+  const SCAN_LINE = 2000000;
+  const unbeaconed = new Set();
 
-      return {
-        sensor: [sx, sy],
-        beacon: [bx, by]
-      };
-    })
-    .map(({ sensor, beacon }) => {
-      const radius = manhattanNorm(subtractVectors(sensor, beacon));
-      return buildCircle(sensor, radius);
-    });
+  readings.forEach(({ sensor, beacon }) => {
+    const radius = manhattanNorm(subtractVectors(sensor, beacon));
+    intersectCircleWithLine(sensor, radius, SCAN_LINE).forEach(v => unbeaconed.add(v));
+  });
+
+  // After the previous pass, we need to remove any items from the unbeaconed list which we know to be beacons.
+  readings.forEach(({ beacon }) => {
+    if (beacon[1] === SCAN_LINE) {
+      unbeaconed.delete(beacon[0]);
+    }
+  });
+
+  console.log(unbeaconed.size);
 
   // Note that this contains duplicates - which aren't a problem for our purposes.
-  const coordsWithoutSensors = sensorCircles.flatMap(v => v);
+  //const coordsWithoutSensors = sensorCircles.flatMap(v => v);
+  //const [width] = computeDimensions(readings.flatMap(r => [r.sensor, r.beacon]));
+  //console.log(width);
 
-  const [width] = computeDimensions(coordsWithoutSensors);
+  //console.log(coordsWithoutSensors);
 
-  console.log(coordsWithoutSensors);
+  // 5286569 too big
 
   return [];
 });
