e4712f954b5e26b6ca1a2fcc5bd8debe279917b5
Cygnut <ligand@hotmail.co.uk>
2022-12-16 01:17:26 +0000

    yet more cleanup

--- src/days/2022/15/index.js
+++ src/days/2022/15/index.js
@@ -3,6 +3,9 @@ import { subtractVectors, manhattanNorm } from '../../../helpers/vector.js';
 import { range } from '../../../helpers/utility.js';
 
 export default day(({ source, isInput }) => {
+  const SCAN_LINE = isInput ? 2000000 : 10;
+  const MAX_DISTANCE = isInput ? 4000000 : 20;
+
   const readings = source.split('\n').map(l => {
     const [, sx, sy, bx, by] =
       /^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/
@@ -25,25 +28,36 @@ export default day(({ source, isInput }) => {
     return range(centre[0] - (radius - yDistance), xRadius);
   };
 
-  const countUnbeaconed = scanLine => {
+  const getUnbeaconed = (y, excludeSensed) => {
     const unbeaconed = new Set();
 
     readings.forEach(({ sensor, beacon }) => {
       const radius = manhattanNorm(subtractVectors(sensor, beacon));
-      intersectCircleWithLine(sensor, radius, scanLine).forEach(v => unbeaconed.add(v));
+      intersectCircleWithLine(sensor, radius, y).forEach(v => unbeaconed.add(v));
     });
 
-    // After the previous pass, we need to remove any items from the unbeaconed list which we know to be beacons.
-    readings.forEach(({ beacon }) => {
-      if (beacon[1] === scanLine) {
-        unbeaconed.delete(beacon[0]);
-      }
-    });
+    if (excludeSensed) {
+      // After the previous pass, we need to remove any items from the unbeaconed list which we know to be beacons.
+      readings.forEach(({ beacon }) => {
+        if (beacon[1] === y) {
+          unbeaconed.delete(beacon[0]);
+        }
+      });
+    }
 
-    return unbeaconed.size;
+    return unbeaconed;
   };
 
-  const SCAN_LINE = isInput ? 2000000 : 10;
+  const countUnbeaconed = () => {
+    return getUnbeaconed(SCAN_LINE, true).size;
+  };
 
-  return [() => countUnbeaconed(SCAN_LINE)];
+  const findBeacon = () => {
+    for (let y = 0; y <= MAX_DISTANCE; ++y) {
+      console.log(y);
+      getUnbeaconed(y, false);
+    }
+  };
+
+  return [() => countUnbeaconed(), () => findBeacon()];
 });
