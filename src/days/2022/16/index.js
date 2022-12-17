6f5685c96c7bdefd1725b7e3f687b890cb5b42c8
Cygnut <ligand@hotmail.co.uk>
2022-12-17 13:21:27 +0000

    wip 16

--- src/days/2022/16/index.js
+++ src/days/2022/16/index.js
@@ -1,25 +1,36 @@
 import solution from '../../../runner/solution.js';
 
 export default solution(({ source }) => {
-  const parsed = source.split('\n').map(l => {
-    console.log(l);
-    const [, valve, flow, to] =
+  const nodes = source.split('\n').map(l => {
+    const [, name, value, to] =
       /^Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.*)$/.exec(l);
 
-    return { valve, flow: parseInt(flow), to: to.split(', ') };
+    return { name, value: parseInt(value), adjacent: to.split(', ') };
   });
 
   const root = 'AA';
 
-  console.log(parsed);
+  console.log(nodes);
 
   // Looking at the data, *most* nodes have a value of 0. The optimal solution would visit as many
   // valued nodes (a value > 0) as possible, using the shortest paths when travelling.
   //
   // So, what we'll do is:
   //   i) 'Eliminate' non-valued nodes by computing the minimum distance between the valued nodes.
-  //   ii) Determine every permutation of valued nodes (which is a far smaller set than the total nodes),
-  //       and maximise the total value over those permutations.
+  //   ii) Go through the possible paths of valued nodes (which is a far smaller tree than the
+  //       total nodes), and maximise the total value over those paths.
+
+  const valuedNodes = nodes.filter(n => n.value);
+
+  nodes.forEach(node => {
+    // Find the shortest path to all valued nodes excluding ourselves
+    node.valuedAdjacent = [];
+    if (!node.value) {
+      return;
+    }
+
+    const valuedNodesNotSelf = valuedNodes.filter(n => n.name !== node.name);
+  });
 
   return [];
 });
