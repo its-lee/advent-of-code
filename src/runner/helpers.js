ac6e74e5ea05cb358dea3adeb21578685661b729
Cygnut <ligand@hotmail.co.uk>
2023-12-09 14:08:22 +0000

    sort tests

--- src/runner/helpers.js
+++ src/runner/helpers.js
@@ -3,5 +3,6 @@ import days from '../days/index.js';
 export const getFilteredDays = (yearDayFilter = null) => {
   return Object.keys(days)
     .filter(yearDay => !yearDayFilter || yearDay === yearDayFilter)
-    .map(yearDay => days[yearDay](yearDay));
+    .map(yearDay => days[yearDay](yearDay))
+    .sort((a, b) => a.id - b.id);
 };
