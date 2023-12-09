8ce4ee915835a1cd10601dd707dc6e6513e97421
Cygnut <ligand@hotmail.co.uk>
2023-12-09 12:44:56 +0000

    fix by being pattern based instead of text based

--- src/days/2023/1/index.js
+++ src/days/2023/1/index.js
@@ -1,36 +1,47 @@
 import solution from '../../../runner/solution.js';
 
 export default solution(({ source }) => {
-  const isNumber = c => !Number.isNaN(Number.parseInt(c));
-
-  const reduceNumbers = numbers => Number.parseInt(numbers.at(0) + numbers.at(-1));
-
-  const reduceString = s => reduceNumbers(s.split('').filter(c => isNumber(c)));
-
   const digitWords = {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9'
+  };
+
+  const digitNumbers = {
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9'
   };
 
   const sumOfDigits = mapWords =>
     source
       .split('\n')
       .map(s => {
-        if (mapWords) {
-          Object.keys(digitWords).forEach(
-            word => (s = s.replace(word, digitWords[word].toString()))
-          );
-        }
-        return s;
+        const digitStrings = { ...digitNumbers, ...(mapWords ? digitWords : undefined) };
+
+        const digitPositions = Object.keys(digitStrings)
+          .flatMap(key =>
+            [...s.matchAll(new RegExp(key, 'g'))].map(m => ({
+              index: m.index,
+              value: digitStrings[key]
+            }))
+          )
+          .sort((a, b) => a.index - b.index);
+
+        return Number.parseInt(digitPositions.at(0).value + digitPositions.at(-1).value);
       })
-      .map(s => reduceString(s))
       .reduce((prev, curr) => prev + curr, 0);
 
   return [() => sumOfDigits(false), () => sumOfDigits(true)];
